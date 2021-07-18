9e68d47a5303f16ffe49f5b029814a29acd6482d
tyrhoads <80553682+tyrhoads@users.noreply.github.com>
2021-07-18 17:05:23 -0400

    added header
    what it said

--- ProgrammingAssignment4.cpp
+++ ProgrammingAssignment4.cpp
@@ -2,8 +2,41 @@
 ///Replace with the date
 ///Replace with the purpose of the program
 
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <iostream>
 
-#include "ProgrammingAssignment4.h"
+
+using namespace std;
+
+struct footBallPlayerType
+{
+    string name;
+    string position;
+    int touchDowns;
+    int catches;
+    int passingYards;
+    int receivingYards;
+    int rushingYards;
+};
+
+const int MAX = 30;
+
+int openFile(ifstream&);
+int openOutFile(ofstream& out);
+void showMenu();
+void getData(ifstream& inf, footBallPlayerType list[], int& length);
+void printPlayerData(footBallPlayerType list[], int length, int playerNum);
+void printData(footBallPlayerType list[], int length);
+void saveData(ofstream& outF, footBallPlayerType list[], int length);
+int searchData(footBallPlayerType list[], int length, string n);
+void updateTouchdowns(footBallPlayerType list[], int touch, int number);
+void updateCatches(footBallPlayerType list[], int c, int number);
+///   update Catches
+///   update Passing Yards
+///   update Receiving Yards
+void updateRushingYards(footBallPlayerType list[], int length, int playerNum, int yards);
 
 int main()
 {
@@ -89,7 +122,7 @@ int main()
             cout << endl;
 
             updateCatches (bigGiants, numOfcatches , playerNum);
-            break;
+            break;  
 
         case 5:
             cout << "Enter player name: ";
